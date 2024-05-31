3fb1192f5062b1e5144bdae36c1b9d7ff2211aac
monadosquito <monadosquito@gmail.com>
2024-05-31 06:54:46 +0300

    fix

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,6 +3,24 @@ import path from 'path'
 import yaml from 'yaml'
 
 
+const HOST = 'localhost'
+const PORT = 8000
+
+
+type Month = number | string
+
+
+const getMonth = async (m: Month) =>
+    await fetch(`http://${HOST}:${PORT}/working-hour/get/?month=${m}`)
+        .then(res => res.json())
+
+const getMonths = async (from: Month, to: Month) =>
+    await fetch(`http://${HOST}:${PORT}/working-hour/get/?from=${from}&to=${to}`)
+        .then(res => res.json())
+
+const clean = async () => await fetch(`http://${HOST}:${PORT}/clean`)
+
+
 const addHoliday = async (h: any) => {
     await fetch('http://localhost:8000/holiday/add', {
         method: 'POST',
@@ -23,35 +41,36 @@ fs.readFile(path.join(root + '/data/holidays-2023.json'), 'utf8')
             .then(JSON.parse)
             .then(res2024 => {
                 (async () => {
-                    for (const h of res2023.holidays) {
+                    await clean()
+                    for await (const h of res2023.holidays) {
                         await addHoliday(h)
                     }
-                    const workingHoursApr2023 = await fetch('http://localhost:8000/working-hour/get/?month=april').then(res => res.json())
-                    const workingHoursMay2023 = await fetch('http://localhost:8000/working-hour/get/?month=may').then(res => res.json())
-                    const workingHoursAprMay2023 = await fetch('http://localhost:8000/working-hour/get/?from=apr?to=may').then(res => res.json())
-                    await fetch('http://localhost:8000/clean')
-                    for (const h of res2024.holidays) {
+                    const workingHoursApr2023 = await getMonth('april')
+                    const workingHoursMay2023 = await getMonth('may')
+                    const workingHoursAprMay2023 = await getMonths('apr', 'may')
+                    await clean()
+                    for await (const h of res2024.holidays) {
                         await addHoliday(h)
                     }
-                    const workingHoursApr2024 = await fetch('http://localhost:8000/working-hour/get/?month=4').then(res => res.json())
-                    const workingHoursMay2024 = await fetch('http://localhost:8000/working-hour/get/?month=5').then(res => res.json())
-                    const workingHoursAprMay2024 = await fetch('http://localhost:8000/working-hour/get/?from=4?to=5').then(res => res.json())
+                    const workingHoursApr2024_1 = await getMonth(4)
+                    const workingHoursMay2024_1 = await getMonth(5)
+                    const workingHoursAprMay2024_1 = await getMonths(4, 5)
 
                     await addHoliday({ name: 'Холидэй', date: '2024-05-06' })
                     await addHoliday({ name: 'Холидэй', date: '2024-05-07' })
                     await addHoliday({ name: 'Холидэй', date: '2024-05-08' })
 
-                    const workingHoursApr2024_2 = await fetch('http://localhost:8000/working-hour/get/?month=4').then(res => res.json())
-                    const workingHoursMay2024_2 = await fetch('http://localhost:8000/working-hour/get/?month=5').then(res => res.json())
-                    const workingHoursAprMay2024_2 = await fetch('http://localhost:8000/working-hour/get/?from=4?to=5').then(res => res.json())
+                    const workingHoursApr2024_2 = await getMonth(4)
+                    const workingHoursMay2024_2 = await getMonth(5)
+                    const workingHoursAprMay2024_2 = await getMonths(4, 5)
 
                     const contents = yaml.stringify({
                         workingHoursApr2023,
                         workingHoursMay2023,
                         workingHoursAprMay2023,
-                        workingHoursApr2024,
-                        workingHoursMay2024,
-                        workingHoursAprMay2024,
+                        workingHoursApr2024_1,
+                        workingHoursMay2024_1,
+                        workingHoursAprMay2024_1,
                         workingHoursApr2024_2,
                         workingHoursMay2024_2,
                         workingHoursAprMay2024_2,
